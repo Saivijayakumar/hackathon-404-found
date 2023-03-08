b1dcb095b50c6c46601049420604dc6fe20831da
Saivijayakumar <81847004+Saivijayakumar@users.noreply.github.com>
2023-03-08 21:10:45 +0530

    final code

---
+++ Speech evaluation/app.py
@@ -0,0 +1,122 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import nltk
+import enchant
+import speech_recognition as sr
+from collections import Counter
+import string
+import textstat
+import spacy
+
+app = Flask(__name__)
+CORS(app)
+
+# Download the necessary NLTK resources
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+
+# Initialize the spell-checker dictionary
+dictionary = enchant.Dict("en_US")
+
+# Load the English language model for spaCy
+nlp = spacy.load("en_core_web_sm")
+
+# global variable to store the sentence
+displayed_sentence = "sai"
+transcript_Data = ""
+
+@app.route('/convert-audio', methods=['POST'])
+def convert_audio():
+    global transcript_Data
+    audio_file = request.files['audio-file']
+    recognizer = sr.Recognizer()
+    audio_data = sr.AudioFile(audio_file)
+    with audio_data as source:
+        audio = recognizer.record(source)
+    text = recognizer.recognize_google(audio)
+    transcript_Data = text
+    return jsonify({'text': text})
+
+@app.route("/check_pronunciation", methods=["POST"])
+def check_pronunciation():
+    global displayed_sentence
+    transcript = request.json["transcript"]
+    global transcript_Data
+    transcript_Data = transcript
+
+    if transcript.upper() == displayed_sentence.upper():
+        success = True
+    else:
+        success = False
+
+    return jsonify({"success": success})
+
+@app.route("/get_spelling")
+def spellingChecker():
+    sentence = transcript_Data
+    print(transcript_Data)
+    words = nltk.word_tokenize(sentence)
+    misspelled_words = []
+    vocab_levels = []
+    for word in words:
+        if not dictionary.check(word) and word not in string.punctuation:
+            misspelled_words.append(word)
+        else:
+            tagged_word = nltk.pos_tag([word])[0]
+            vocab_levels.append(tagged_word[1])
+    #print(vocab_levels)
+    vocab_counts = Counter(vocab_levels)
+    total_words = len(vocab_levels)
+    results = {"misspelled_words": misspelled_words}
+    for level, count in vocab_counts.items():
+        #print("inside for loop ")
+        if level == 'DT':
+            results['Determiners'] = round(count / total_words * 100, 2)
+        elif level == 'JJ':
+            results['Adjectives'] = round(count / total_words * 100, 2)
+        elif level == 'NN':
+            results['Nouns'] = round(count / total_words * 100, 2)
+        elif level == 'IN':
+            results['Prepositions or subordinating conjunctions'] = round(count / total_words * 100, 2)
+        elif level == 'CC':
+            results['Coordinating conjunctions'] = round(count / total_words * 100, 2)
+        elif level == 'NNS':
+            results['Plural nouns'] = round(count / total_words * 100, 2)
+        elif level == '.':
+            results['Punctuation marks'] = round(count / total_words * 100, 2)
+        elif level == 'VBP':
+            results['Non-3rd person singular present verbs'] = round(count / total_words * 100, 2)
+        elif level == 'VBD':
+            results['Past tense verbs'] = round(count / total_words * 100, 2)
+    return jsonify(results)
+
+@app.route("/sentencescore")
+def lexicon_score():
+
+    # Get the Flesch-Kincaid Grade Level of the sentence
+    grade_level = textstat.flesch_kincaid_grade(transcript_Data)
+
+    # Tokenize the sentence using spaCy
+    doc = nlp(transcript_Data)
+    wordcount = len(doc)
+    print("length in complex",len(doc))
+    complex_words = []
+    complex_phrases = []
+
+    for token in doc:
+        
+        if textstat.syllable_count(token.text) >= 3:
+            complex_words.append(token.text)
+        
+        if token.dep_ == "ROOT":
+            if textstat.text_standard(token.text, float_output=True) >= 10:
+                complex_phrases.append(token.text)
+    results = {"complex_words": complex_words}
+    results['complex_phrases'] = complex_phrases
+    results['grade_level'] = grade_level
+    results['wordcount'] = wordcount
+    return jsonify(results)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ Speech evaluation/index.js
@@ -0,0 +1,192 @@
+const startRecordingEl = document.querySelector("#start-recording");
+const stopRecordingEl = document.querySelector("#stop-recording");
+const resultEl = document.querySelector("#result");
+const Transcribedata = document.querySelector("#Transcribe");
+const scoreresult = document.querySelector("#score");
+const misspelled_result = document.querySelector("#misspelled_result");
+const complex_phrases = document.querySelector("#complex_phrases");
+const audioContainer = document.querySelector('.audio-container');
+const buttonContainer = document.querySelector('.button-container');
+const toggleButton = document.querySelector('#toggle-button');
+
+
+disableContainer(audioContainer);
+enableContainer(buttonContainer);
+
+function convertAudio() {
+  resultEl.innerHTML = "Processing audio...";
+  var input = document.getElementById('audio-file');
+  var file = input.files[0];
+  var formData = new FormData();
+  formData.append('audio-file', file);
+  fetch('http://127.0.0.1:5000/convert-audio', {
+    method: 'POST',
+    body: formData
+  })
+  .then(response => response.json())
+  .then(data => {
+    Transcribedata.innerHTML = "Transcribe";
+    var resultDiv = document.getElementById('result');
+    resultDiv.innerText = data.text;
+    console.log("complex function")
+    complex_function();
+    console.log("spelling function")
+    spellingcheck();
+  }).catch(error => {
+    resultEl.innerHTML = "Error while uploading the file. Please upload the correct file.";
+    console.error(error);
+  });
+
+}
+
+const recognition = new webkitSpeechRecognition();
+recognition.continuous = true;
+recognition.interimResults = false;
+recognition.lang = 'en-US'
+
+let transcript = '';
+let wordcount = '';
+
+startRecordingEl.addEventListener("click", function() {
+  recognition.start();
+  transcript = '';
+  resultEl.innerHTML = "Recording...";
+});
+
+stopRecordingEl.addEventListener("click", function() {
+  recognition.stop();
+  resultEl.innerHTML = "Processing...";
+  if (!transcript) {
+    resultEl.innerHTML = "Your audio was not recorded.";
+    return;
+  }
+
+  fetch("http://127.0.0.1:5000/check_pronunciation", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ transcript })
+  })
+    .then(response => response.json())
+    .then(result => {
+      if (result.success) {
+        resultEl.innerHTML = "Response is the same";
+      } else {
+        Transcribedata.innerHTML = "Transcribe";
+        resultEl.innerHTML = transcript;
+        spellingcheck();
+        complex_function();
+  }
+    })
+    .catch(error => {
+      console.error(error);
+    });
+});
+
+
+recognition.onresult = function(event) {
+  const newTranscription = event.results[event.results.length - 1][0].transcript;
+  transcript += newTranscription;
+  if (!transcript) {
+    resultEl.innerHTML = "Not Recording";
+  }
+};
+
+
+function complex_function(){
+  fetch("http://127.0.0.1:5000/sentencescore")
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    let sentencescore = "";
+    if (data.complex_phrases.length > 0) {
+      const complex_phr = data.misspelled_words.join(", ");
+      complex_phrase_result = "<br>complex phrases in Transcribe are: " + complex_phr;
+    } else {
+      complex_phrase_result = "<br>No complex phrases in Transcribe";
+    }
+    if (data.complex_words.length > 0) {
+      const complex_wor = data.complex_words.join(", ");
+      complex_word_result = "<br>complex Words in Transcribe are: " + complex_wor;
+    } else {
+      complex_word_result = "<br>No complex words in Transcribe";
+    }
+    scoreresult.innerHTML =  "Readability score for Transcribe: "+data.grade_level;
+    complex_phrases.innerHTML = complex_word_result + complex_phrase_result;
+    wordcount = data.wordcount;
+    console.log("now you see the length")
+    console.log(data.wordcount)
+  })
+  .catch(error => {
+    console.error(error);
+  });
+}
+
+function spellingcheck(){
+  fetch("http://127.0.0.1:5000/get_spelling")
+  .then(response => response.json())
+  .then(data => {
+    // Display misspelled_words
+    console.log(data)
+    let misspelledWordsResult = "";
+    if (data.misspelled_words.length > 0) {
+      const misspelledWords = data.misspelled_words.join(", ");
+      misspelledWordsResult = "<br>Misspelled words in Transcribe are: " + misspelledWords;
+    } else {
+      misspelledWordsResult = "<br>No misspelled words present for above Transcribe";
+    }
+    
+    // Display other results
+    let otherResults = "";
+    Object.keys(data).forEach(key => {
+      if (key !== "misspelled_words") {
+        const value = data[key];
+        otherResults += "<br>" + key + ": " + value + "%";
+      }
+    });
+    
+    // Display both results
+    misspelled_result.innerHTML = misspelledWordsResult +"<hr>"+"Percentage of parts of speech out of "+ wordcount+" words in Transcribe"+
+    "<br>" +otherResults;
+    
+  })
+  .catch(error => {
+    console.error(error);
+  });
+}
+toggleButton.addEventListener('change', function() {
+  if (this.checked) {
+    disableContainer(buttonContainer);
+    enableContainer(audioContainer);
+    emptyall();
+    document.querySelector('#audio-file').value = '';
+  } else {
+    disableContainer(audioContainer);
+    enableContainer(buttonContainer);
+    emptyall();
+    document.querySelector('#audio-file').value = '';
+
+  }
+});
+
+function disableContainer(container) {
+  const inputs = container.querySelectorAll('input, button');
+  inputs.forEach(input => {
+    input.disabled = true;
+  });
+}
+
+function enableContainer(container) {
+  const inputs = container.querySelectorAll('input, button');
+  inputs.forEach(input => {
+    input.disabled = false;
+  });
+}
+function emptyall(){
+  misspelled_result.innerHTML = "";
+  Transcribedata.innerHTML = "";
+  scoreresult.innerHTML = "";
+  complex_phrases.innerHTML  = "";
+  resultEl.innerHTML = "";
+  Transcribedata.innerHTML = "";
+}
+
